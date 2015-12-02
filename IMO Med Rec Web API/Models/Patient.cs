00c5ff1c9e02ca8ceb539c04e731426f7776f48b
muqizou <muqizou2@illinois.edu>
2015-12-02 17:12:13 -0600

    Create Patient.cs

---
+++ IMO Med Rec Web API/Models/Patient.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMO_Med_Rec_Web_API.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Allergy { get; set; }
+
+    }
+}
